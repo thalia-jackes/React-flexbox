122600b89620d1f29e9655475686e36e36957f59
unknown <jackesthalia@gmail.com>
2021-03-28 14:00:18 -0300

    imagens

--- src/components/Produtos/index.js
+++ src/components/Produtos/index.js
@@ -8,15 +8,15 @@ function Produtos(){
       <div className="produtos-container">
         <div className="produtos-item purple">
           <h2> Purple</h2>
-          <img src="img/sobre4.jpg" alt=" Produtos 1"/>
+          <img src="img/produto1.jpg" alt=" Produtos 1"/>
         </div>
         <div className="produtos-item pink">
           <h2> Pink</h2>
-          <img src="img/sobre3.jpg" alt=" Produtos 2"/>
+          <img src="img/produto2.jpg" alt=" Produtos 2"/>
         </div>
         <div className="produtos-item blue">
           <h2> Blue</h2>
-          <img src="img/sobre2.jpg" alt=" Produtos 3"/>
+          <img src="img/produto3.jpg" alt=" Produtos 3"/>
         </div>
       </div>
     </section>
